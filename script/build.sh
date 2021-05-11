f7e7b5b1a8e688366ca6757eafe60993ecbd26cb
gerald Prendi <gerald.prendi@usi.ch>
2021-05-12 00:38:33 +0200

    Added script to run files

--- script/build.sh
+++ script/build.sh
@@ -1,5 +1,10 @@
-#/bin/sh
+#!/bin/bash
 
-mvn clean package
-mv target/jtc-1.0-SNAPSHOT.jar bin/jtc-1.0.jar
+BASE_DIR=/home/geri/Desktop/University/PF2/HOMEWORK/project-jtc/
+TARGET_FILE=target/jtc-1.0-SNAPSHOT.jar
+FINAL_FILE=bin/jtc-1.0.jar
+
+cd $BASE_DIR
+mvn clean package && mv $TARGET_FILE $FINAL_FILE && echo Project built and packaged. Packaged jar is saved at $FINAL_FILE
+
 
---
+++ script/run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+BASE_DIR=/home/geri/Desktop/University/PF2/HOMEWORK/project-jtc/
+
+cd $BASE_DIR
+MAIN_CLASS=$(cat pom.xml | grep mainClass | sed 's/<mainClass>//g' | sed 's/<\/mainClass>//g' | awk '{print $1}')
+echo $MAIN_CLASS
+mvn compile
+mvn exec:java -Dexec.mainClass="$MAIN_CLASS"
