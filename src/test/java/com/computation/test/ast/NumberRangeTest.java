98108af026f5d6e0149cfe007b9015dab020bcf3
Enrico <dipiee@usi.ch>
2021-06-03 00:47:27 +0200

    Minor fixes after merge

--- src/test/java/com/computation/test/ast/NumberRangeTest.java
+++ src/test/java/com/computation/test/ast/NumberRangeTest.java
@@ -4,6 +4,7 @@ import com.computation.ast.Type;
 import com.computation.ast.intnodes.IntLiteral;
 import com.computation.ast.range.NumberRange;
 import com.computation.ast.range.Range;
+import com.computation.ast.range.RangeException;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -12,7 +13,7 @@ import static org.junit.Assert.assertFalse;
 public class NumberRangeTest {
 
     @Test
-    public void testNumberRange() {
+    public void testNumberRange() throws RangeException {
         NumberRange nr = new NumberRange(new IntLiteral(1), new IntLiteral(2));
 
         // TODO finish this
