3883f1703ba8fdd74b23ab349229abb6d52e2607
gerald Prendi <gerald.prendi@usi.ch>
2021-06-03 00:40:05 +0200

    Fixed cpd error

--- src/main/java/com/spreadsheetview/gui/center/ColorRenderer.java
+++ src/main/java/com/spreadsheetview/gui/center/ColorRenderer.java
@@ -7,15 +7,19 @@ import javax.swing.table.DefaultTableCellRenderer;
 
 class ColorRenderer extends DefaultTableCellRenderer {
 
+    private final Color selectionColor = new Color(172, 225, 175);
+
     @Override
     public Component getTableCellRendererComponent(
             final JTable table,
             final Object value, final boolean isSelected,
             final boolean hasFocus,final int row,final int column) {
+
+
         super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
 
         if (isSelected) {
-            setBackground(new Color(172, 225, 175));
+            setBackground(selectionColor);
         } else {
             setBackground(table.getBackground());
         }
